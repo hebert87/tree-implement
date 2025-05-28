1819a60f5cb0853abfddbd8ea80b7ecd4c5be8e2
Jessica H <jessicahebert75@gmail.com>
2025-05-28 11:27:08 -0700

    added a sum method that sums the whole tree

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,7 @@
 
 
+
+
 public class Traversal {
   public static void main(String[] args) {
     TreeNode root = new TreeNode(10);
@@ -7,11 +9,13 @@ public class Traversal {
     root.right = new TreeNode(15);
     root.left.left = new TreeNode(5);
     root.left.right = new TreeNode(2);
-    root.right = new TreeNode(15);
+    root.right.left = new TreeNode(15);
     root.left = new TreeNode(-3);
     root.right.right = new TreeNode(5);
     root.right.right.right = new TreeNode(22);
-    preOrder(root);
+    //preOrder(root);
+    int results = sum(root);
+    System.out.println(results);
   }
   public static void preOrder(TreeNode current){
     // null return ,print our data, search left, search right
@@ -23,5 +27,18 @@ public class Traversal {
     preOrder(current.left);
     preOrder(current.right );
     
+  }
+  public static  int sum(TreeNode node){
+    // if null -> 0
+    if (node == null) return 0;
+    // else my calue + sum of left subtree + sum right subtree
+    // start total at root
+    int total = node.data;
+
+    // add sum of left subtree to total
+    total+= sum(node.left);
+    total+=sum(node.right);
+    return total;
+
   }
 }
